3d5e2174204e9325fb283ce53c89bf927a18e52a
Manav Mendiratta <mmendiratta@wisc.edu>
2021-02-25 20:56:23 -0600

    additions to profile/account page

--- screens/account/AccountScreen.tsx
+++ screens/account/AccountScreen.tsx
@@ -1,14 +1,70 @@
 import * as React from 'react';
 import { styles } from '../../styles/stylesheet'
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
 
 export default function AccountScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Account Page</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/AccountScreen.tsx" />
-    </View>
+          <View style={styles.accountHeader}></View>
+          <Image style={styles.accountAvatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={{marginTop: 80}}>
+            <View style={styles.accountContent}>
+              {/* <Text style={styles.accountName}>John Doe</Text>
+              <Text style={styles.accountInfo}>Software Engineer</Text>
+              <Text style={styles.accountDescription}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+              
+              <TouchableOpacity style={styles.accountButtonContainer}>
+                <Text>Option 1</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.accountButtonContainer}>
+                <Text>Option 2</Text> 
+              </TouchableOpacity> */}
+                 <Text style={styles.accountName}>John Doe </Text>
+                <Text style={styles.userInfo}>jhonnydoe@mail.com </Text>
+                <Text style={styles.userInfo}>Florida </Text>
+               <View style={styles.item}>
+              <View style={styles.iconContent}>
+                <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/70/000000/cottage.png'}}/>
+              </View>
+              <View style={styles.infoContent}>
+                <Text style={styles.info}>Home</Text>
+              </View>
+            </View>
+
+            <View style={styles.item}>
+              <View style={styles.iconContent}>
+                <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/70/000000/administrator-male.png'}}/>
+              </View>
+              <View style={styles.infoContent}>
+                <Text style={styles.info}>Settings</Text>
+              </View>
+            </View>
+
+            <View style={styles.item}>
+              <View style={styles.iconContent}>
+                <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/70/000000/filled-like.png'}}/>
+              </View>
+              <View style={styles.infoContent}>
+                <Text style={styles.info}>News</Text>
+              </View>
+            </View>
+
+            <View style={styles.item}>
+              <View style={styles.iconContent}>
+                <Image style={styles.icon} source={{uri: 'https://img.icons8.com/color/70/000000/facebook-like.png'}}/>
+              </View>
+              <View style={styles.infoContent}>
+                <Text style={styles.info}>Shop</Text>
+              </View>
+            </View>
+            </View>
+        </View>
+      </View>
   );
 }
--- styles/stylesheet.tsx
+++ styles/stylesheet.tsx
@@ -57,4 +57,79 @@ carouselContainer: {
     height: 3,
     width: '80%',
   },
+  accountHeader:{
+    backgroundColor: "#00BFFF",
+    height:200,
+  },
+  accountAvatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    // marginBottom:10,
+    alignSelf:'center',
+    // position: 'absolute',
+    // marginTop:130
+  },
+  accountContent: {
+    flex: 1,
+    alignItems: 'center',
+    //padding:30,
+  },
+  accountName:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  accountInfo:{
+    fontSize:16,
+    color: "#33DAFF",
+    marginTop:10
+  },
+  accountDescription:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  accountButtonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#33DAFF",
+  },
+  userInfo:{
+    fontSize:16,
+    color:"#778899",
+    fontWeight:'600',
+  },
+  item:{
+    flexDirection : 'row',
+  },
+  infoContent:{
+    flex:1,
+    alignItems:'flex-start',
+    paddingLeft:5
+  },
+  iconContent:{
+    flex:1,
+    alignItems:'flex-end',
+    paddingRight:5,
+  },
+  icon:{
+    width:30,
+    height:30,
+    marginTop:20,
+  },
+  info:{
+    fontSize:18,
+    marginTop:20,
+    color: "#FFFFFF",
+  }
 });
