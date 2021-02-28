66e33bdbf3192952606d337ef63e4310202d3c62
Manav Mendiratta <mmendiratta@wisc.edu>
2021-02-28 12:47:34 -0600

    started on search shop

--- screens/shop/ShopScreen.tsx
+++ screens/shop/ShopScreen.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
+import { SearchBar } from 'react-native-elements';
 import { styles } from '../../styles/stylesheet'
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-export default function ShopScreen() {
+const ShopScreen = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Shop Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.header}>Search Bar</Text>
-      <Text style={styles.header}>Club Type</Text>
-      <Text style={styles.header}>Equipment</Text>
-      <Text style={styles.header}>Clothing</Text>
-      <Text style={styles.header}>Technology</Text>
-    </View>
+    <SearchBar
+      placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
   );
-}
+};
+
+export default ShopScreen;
